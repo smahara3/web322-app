2ce1258f8b1a3a2c728194e069dec19552cea247
Smriti Mahara <smahara3@seneca.ca>
2024-10-31 19:35:41 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 /*********************************************************************************
 
-WEB322 – Assignment 02
+WEB322 – Assignment 03
 I declare that this assignment is my own work in accordance with Seneca Academic Policy.  No part 
 of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 
 Name: Smriti Mahara
 Student ID: 120885231
-Date: 10/09/2024
+Date: 10/28/2024
 Cyclic Web App URL: https://aback-chivalrous-piano.glitch.me/about
 GitHub Repository URL: https://github.com/smahara3/web322-app
 
 ********************************************************************************/
+
 
 const express = require("express");
 const path = require("path");
